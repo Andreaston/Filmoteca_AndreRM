82b56b40a9878de6a185445452bf4b7b4f30f979
Andreaston <andre.r.m@hotmail.es>
2025-12-04 19:15:17 +0100

    registro falta estado

--- app/src/main/java/com/example/filmoteca_andrerm/RegistroActivity.java
+++ app/src/main/java/com/example/filmoteca_andrerm/RegistroActivity.java
@@ -1,4 +1,63 @@
 package com.example.filmoteca_andrerm;
 
-public class RegistroActivity {
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class RegistroActivity extends AppCompatActivity {
+
+
+    protected void onCreate(Bundle x){
+        //Ligamos con el layout
+        super.onCreate(x);
+        setContentView(R.layout.activity_registro);
+
+        //vinculamos los elementos
+        EditText userText = findViewById(R.id.userName);
+        EditText emailText = findViewById(R.id.regEmail);
+        EditText passText = findViewById(R.id.regPassword);
+        EditText passConfirmText = findViewById(R.id.regPasswordConfirm);
+        EditText tlfText = findViewById(R.id.numberTlf);
+        Button botonConfirmar = findViewById(R.id.btnRegistrar);
+
+        //Estado
+        SharedPreferences preferences = null;
+
+        //boton confirmar
+        botonConfirmar.setOnClickListener(view -> {
+
+            String user = userText.getText().toString().trim();
+            String email = emailText.getText().toString().trim();
+            String pass = passText.getText().toString().trim();
+            String passConfirm = passConfirmText.getText().toString().trim();
+            String tlf = tlfText.getText().toString().trim();
+            //Error campos vacios
+            if (user.isEmpty() || email.isEmpty() || pass.isEmpty() || passConfirm.isEmpty() || tlf.isEmpty() ){
+
+                Toast.makeText(this,getString(R.string.register_error_empty),Toast.LENGTH_SHORT).show();
+                return;
+            }
+            //Error contraseñas diferentes
+            if (!passConfirm.equals(pass)){
+                Toast.makeText(this,getString(R.string.error_password),Toast.LENGTH_SHORT).show();
+                return;
+            }
+            //Email existente
+
+            String emailGuardados = preferences.getString("registered_email",null);
+
+            if (email.equals(DEFAULT_EMAIL) || (emailGuardados != null && emailText.equals(emailGuardados))){
+                return;
+            }
+
+        });
+
+
+    }
+
+
 }
